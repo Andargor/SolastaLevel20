653a5d34b7ea327b07ed7e505f97a115585d6aa2
ThyWoof <pauloesquilo@gmail.com>
2021-05-29 18:43:54 -0700

    initial commit by SolastaMod

---
+++ SolastaLevel20/Patches/GameManagerPatcher.cs
@@ -0,0 +1,16 @@
+using HarmonyLib;
+
+namespace SolastaLevel20.Patches
+{
+    class GameManagerPatcher
+    {
+        [HarmonyPatch(typeof(GameManager), "BindPostDatabase")]
+        internal static class GameManager_BindPostDatabase_Patch
+        {
+            internal static void Postfix()
+            {
+                Main.ModEntryPoint();
+            }
+        }
+    }
+}
