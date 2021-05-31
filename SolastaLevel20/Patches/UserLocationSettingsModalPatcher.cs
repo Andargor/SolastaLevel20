0805804ce04860bb6da39ab698733de67cd4df39
ThyWoof <pauloesquilo@gmail.com>
2021-05-31 15:41:54 -0700

    use a transpiler instead

--- SolastaLevel20/Patches/UserLocationSettingsModalPatcher.cs
+++ SolastaLevel20/Patches/UserLocationSettingsModalPatcher.cs
@@ -1,41 +1,68 @@
 ﻿using TMPro;
+using System.Collections.Generic;
 using HarmonyLib;
 
 namespace SolastaLevel20.Patches
 {
     class UserLocationSettingsModalPatcher
     {
+        //[HarmonyPatch(typeof(UserLocationSettingsModal), "OnMinLevelEndEdit")]
+        //internal static class UserLocationSettingsModal_OnMinLevelEndEdit_Patch
+        //{
+        //    internal static bool Prefix(TMP_InputField ___minLevelInputField, int ___minLevel)
+        //    {
+        //        if (!int.TryParse(___minLevelInputField.text, out int result))
+        //            result = 1;
+        //        else if (result < 0)
+        //            result = 1;
+        //        else if (result > Main.MAX_LEVEL)
+        //            result = Main.MAX_LEVEL;
+        //        ___minLevelInputField.text = result.ToString();
+        //        ___minLevel = result;
+        //        return false;
+        //    }
+        //}
         [HarmonyPatch(typeof(UserLocationSettingsModal), "OnMinLevelEndEdit")]
-        internal static class UserLocationSettingsModalPatcher_OnMinLevelEndEdit_Patch
+        public class UserLocationSettingsModal_OnMinLevelEndEdit_Patch
         {
-            internal static bool Prefix(TMP_InputField ___minLevelInputField, int ___minLevel)
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                if (!int.TryParse(___minLevelInputField.text, out int result))
-                    result = 1;
-                else if (result < 0)
-                    result = 1;
-                else if (result > Main.MAX_LEVEL)
-                    result = Main.MAX_LEVEL;
-                ___minLevelInputField.text = result.ToString();
-                ___minLevel = result;
-                return false;
+                var code = new List<CodeInstruction>(instructions);
+                var opcodes = code.FindAll(x => x.opcode.Name == "ldc.i4.s" && x.operand.ToString() == "10");
+                foreach (var opcode in opcodes)
+                    opcode.operand = 20;
+
+                return code;
             }
         }
 
+        //[HarmonyPatch(typeof(UserLocationSettingsModal), "OnMaxLevelEndEdit")]
+        //internal static class UserLocationSettingsModal_OnMaxLevelEndEdit_Patch
+        //{
+        //    internal static bool Prefix(TMP_InputField ___maxLevelInputField, int ___maxLevel)
+        //    {
+        //        if (!int.TryParse(___maxLevelInputField.text, out int result))
+        //            result = 1;
+        //        else if (result < 0)
+        //            result = 1;
+        //        else if (result > Main.MAX_LEVEL)
+        //            result = Main.MAX_LEVEL;
+        //        ___maxLevelInputField.text = result.ToString();
+        //        ___maxLevel = result;
+        //        return false;
+        //    }
+        //}
         [HarmonyPatch(typeof(UserLocationSettingsModal), "OnMaxLevelEndEdit")]
-        internal static class UserLocationSettingsModalPatcher_OnMaxLevelEndEdit_Patch
+        public class UserLocationSettingsModal_OnMaxLevelEndEdit_Patch
         {
-            internal static bool Prefix(TMP_InputField ___maxLevelInputField, int ___maxLevel)
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                if (!int.TryParse(___maxLevelInputField.text, out int result))
-                    result = 1;
-                else if (result < 0)
-                    result = 1;
-                else if (result > Main.MAX_LEVEL)
-                    result = Main.MAX_LEVEL;
-                ___maxLevelInputField.text = result.ToString();
-                ___maxLevel = result;
-                return false;
+                var code = new List<CodeInstruction>(instructions);
+                var opcodes = code.FindAll(x => x.opcode.Name == "ldc.i4.s" && x.operand.ToString() == "10");
+                foreach (var opcode in opcodes)
+                    opcode.operand = 20;
+
+                return code;
             }
         }
     }
