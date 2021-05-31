d7374264792536f2609b878f7f45a8ac66e23739
ThyWoof <pauloesquilo@gmail.com>
2021-05-31 11:32:12 -0700

    level 10 hardcoded everywhere. oh damm const

---
+++ SolastaLevel20/Patches/CharacterBuildingManagerPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace SolastaLevel20.Patches
+{
+    class CharacterBuildingManagerPatcher
+    {
+        [HarmonyPatch(typeof(CharacterBuildingManager), "CreateCharacterFromTemplate")]
+        public class CharacterBuildingManager_CreateCharacterFromTemplate_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var code = new List<CodeInstruction>(instructions);
+                code[4].operand = Main.MAX_LEVEL;
+                return code;
+            }
+        }
+    }
+}
