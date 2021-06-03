6907bbcc2f66f6445a04e68378fe4d23ce4c2c19
Paulo Monteiro <pauloesquilo@gmail.com>
2021-06-03 16:10:54 -0700

    clean up

--- SolastaLevel20/Patches/CharactersPanelPatcher.cs
+++ SolastaLevel20/Patches/CharactersPanelPatcher.cs
@@ -6,15 +6,6 @@ namespace SolastaLevel20.Patches
 {
     class CharactersPanelPatcher
     {
-        //[HarmonyPatch(typeof(CharactersPanel), "Refresh")]
-        //internal static class CharactersPanel_Refresh_Patch
-        //{
-        //    internal static void Postfix(CharactersPanel __instance, Button ___levelUpButton, int ___selectedPlate, List<CharacterPlateToggle> ___characterPlates)
-        //    {
-        //        ___levelUpButton.interactable = ___selectedPlate >= 0 && ___selectedPlate < ___characterPlates.Count && ___characterPlates[___selectedPlate].GuiCharacter.CharacterLevel < Main.MOD_MAX_LEVEL;
-        //    }
-        //}
-
         [HarmonyPatch(typeof(CharactersPanel), "Refresh")]
         internal static class CharactersPanel_Refresh_Patch
         {
