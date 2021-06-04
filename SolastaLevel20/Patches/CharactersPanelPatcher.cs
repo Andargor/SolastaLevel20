6cddb96506ae0afc48892582eecd41a3c17e3418
Paulo Monteiro <pauloesquilo@gmail.com>
2021-06-04 06:52:59 -0700

    refactor

--- SolastaLevel20/Patches/CharactersPanelPatcher.cs
+++ SolastaLevel20/Patches/CharactersPanelPatcher.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.UI;
 using HarmonyLib;
+
 
 namespace SolastaLevel20.Patches
 {
@@ -13,15 +15,15 @@ namespace SolastaLevel20.Patches
             static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 var code = new List<CodeInstruction>(instructions);
-                code.Find(x => x.opcode.Name == "ldc.i4.s" && x.operand.ToString() == Main.GAME_MAX_LEVEL).operand = Main.MOD_MAX_LEVEL;
+                code.Find(x => x.opcode.Name == "ldc.i4.s" && Convert.ToInt32(x.operand) == Main.GAME_MAX_LEVEL).operand = Main.MOD_MAX_LEVEL;
                 return code;
             }
 
             static void Postfix(CharactersPanel __instance, Button ___exportPdfButton, List<CharacterPlateToggle> ___characterPlates, int ___selectedPlate)
             {
 
-                var characterLevel = (___selectedPlate >= 0) ? ___characterPlates[___selectedPlate].GuiCharacter.CharacterLevel : 1;
-                ___exportPdfButton.gameObject.SetActive(characterLevel > 1);
+                var characterLevel = (___selectedPlate >= 0) ? ___characterPlates[___selectedPlate].GuiCharacter.CharacterLevel : Main.MOD_MIN_LEVEL;
+                ___exportPdfButton.gameObject.SetActive(characterLevel > Main.MOD_MIN_LEVEL);
             }
         }
 
@@ -38,21 +40,20 @@ namespace SolastaLevel20.Patches
                 service.LoadCharacter(filename, out hero, out snapshot);
                 var exp = hero.ExperienceGained;
                 var characterLevel = hero.GetAttribute("CharacterLevel").CurrentValue.ToString();
+                //if (characterLevel == "1")
+                //    __instance.OnNewCharacterCb();
+                //else
+                //{
                 hero.RemoveClassLevel();
                 hero.ExperienceGained = exp;
-                foreach (var key in hero.ActiveFeatures.Where(x => x.Key.EndsWith(characterLevel)).Select(x => x.Key).ToList())
+                var featuresToRemove = hero.ActiveFeatures.Where(x => x.Key.EndsWith(characterLevel)).Select(x => x.Key);
+                foreach (var key in featuresToRemove)
                     hero.ActiveFeatures.Remove(key);
                 hero.RefreshAll();
                 hero.FillSnapshot(snapshot, true);
                 service.Pool.Remove(filename);
                 service.SaveCharacter(hero, true);
-                //___characterPlates[___selectedPlate].Bind(
-                //    filename,
-                //    snapshot,
-                //    ___characterPlates[___selectedPlate].OnPlateSelected,
-                //    ___characterPlates[___selectedPlate].OnPlateDoubleClicked,
-                //    ___characterPlates[___selectedPlate].OnPlateDeleted,
-                //    true);
+                //}
             }
         }        
     }
