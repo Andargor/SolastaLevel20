1b32d71b255d2a9a59490fb47eb742ef9eb1a9eb
Paulo Monteiro <pauloesquilo@gmail.com>
2021-06-03 21:06:51 -0700

    clean up

--- SolastaLevel20/Patches/UserLocationSettingsModalPatcher.cs
+++ SolastaLevel20/Patches/UserLocationSettingsModalPatcher.cs
@@ -6,23 +6,6 @@ namespace SolastaLevel20.Patches
 {
     class UserLocationSettingsModalPatcher
     {
-        //[HarmonyPatch(typeof(UserLocationSettingsModal), "OnMinLevelEndEdit")]
-        //internal static class UserLocationSettingsModal_OnMinLevelEndEdit_Patch
-        //{
-        //    internal static bool Prefix(TMP_InputField ___minLevelInputField, int ___minLevel)
-        //    {
-        //        if (!int.TryParse(___minLevelInputField.text, out int result))
-        //            result = 1;
-        //        else if (result < 0)
-        //            result = 1;
-        //        else if (result > Main.MOD_MAX_LEVEL)
-        //            result = Main.MOD_MAX_LEVEL;
-        //        ___minLevelInputField.text = result.ToString();
-        //        ___minLevel = result;
-        //        return false;
-        //    }
-        //}
-
         [HarmonyPatch(typeof(UserLocationSettingsModal), "OnMinLevelEndEdit")]
         public class UserLocationSettingsModal_OnMinLevelEndEdit_Patch
         {
@@ -36,23 +19,6 @@ namespace SolastaLevel20.Patches
                 return code;
             }
         }
-
-        //[HarmonyPatch(typeof(UserLocationSettingsModal), "OnMaxLevelEndEdit")]
-        //internal static class UserLocationSettingsModal_OnMaxLevelEndEdit_Patch
-        //{
-        //    internal static bool Prefix(TMP_InputField ___maxLevelInputField, int ___maxLevel)
-        //    {
-        //        if (!int.TryParse(___maxLevelInputField.text, out int result))
-        //            result = 1;
-        //        else if (result < 0)
-        //            result = 1;
-        //        else if (result > Main.MOD_MAX_LEVEL)
-        //            result = Main.MOD_MAX_LEVEL;
-        //        ___maxLevelInputField.text = result.ToString();
-        //        ___maxLevel = result;
-        //        return false;
-        //    }
-        //}
 
         [HarmonyPatch(typeof(UserLocationSettingsModal), "OnMaxLevelEndEdit")]
         public class UserLocationSettingsModal_OnMaxLevelEndEdit_Patch
