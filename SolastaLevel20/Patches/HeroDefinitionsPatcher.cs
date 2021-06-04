e25149b664ce8c7b635d1745f319be170a1912bb
Paulo Monteiro <pauloesquilo@gmail.com>
2021-06-03 21:07:09 -0700

    patch MaxHeroExperience

---
+++ SolastaLevel20/Patches/HeroDefinitionsPatcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace SolastaLevel20.Patches
+{
+    class HeroDefinitionsPatcher
+    {
+        [HarmonyPatch(typeof(HeroDefinitions), "MaxHeroExperience")]
+        internal static class HeroDefinitions_MaxHeroExperience_Patch
+        {
+            static bool Prefix(ref int __result)
+            {
+                __result = RuleDefinitions.ExperienceThresholds[Main.MOD_MAX_LEVEL - 1] - 1;
+                return false;
+            }
+        }
+    }
+}
