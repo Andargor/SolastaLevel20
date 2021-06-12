c3b46c5a799fb8229b04cbccd25f285d35534147
leprechaun7377 <chris.p.john@gmail.com>
2021-06-11 23:05:06 -0700

    Fix rogue sneak attack at levels above 10.

--- SolastaLevel20/Rules/Classes/RogueBuilder.cs
+++ SolastaLevel20/Rules/Classes/RogueBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using SolastaModApi;
+using SolastaModApi.Infrastructure;
+using System.Collections.Generic;
 using static SolastaModApi.DatabaseHelper.CharacterClassDefinitions;
 using static SolastaModApi.DatabaseHelper.FeatureDefinitionFeatureSets;
 
@@ -21,6 +23,28 @@ namespace SolastaLevel20.Rules.Classes
                 new FeatureUnlockByLevel(FeatureSetAbilityScoreChoice, 19)
                 // TODO 20: Stroke of Luck
             });
+            // In case TA adds levels 11-20 later on, clear them out. If that happens we can delete this section.
+            DatabaseHelper.FeatureDefinitionAdditionalDamages.AdditionalDamageRogueSneakAttack.DiceByRankTable.RemoveAll(x => x.Rank > 10);
+            DatabaseHelper.FeatureDefinitionAdditionalDamages.AdditionalDamageRogueSneakAttack.DiceByRankTable.AddRange(new List<DiceByRank>() {
+                BuildDiceByRank(11, 6),
+                BuildDiceByRank(12, 6),
+                BuildDiceByRank(13, 7),
+                BuildDiceByRank(14, 7),
+                BuildDiceByRank(15, 8),
+                BuildDiceByRank(16, 8),
+                BuildDiceByRank(17, 9),
+                BuildDiceByRank(18, 9),
+                BuildDiceByRank(19, 10),
+                BuildDiceByRank(20, 10),
+            });;
         }
+        private static DiceByRank BuildDiceByRank(int rank, int dice)
+        {
+            DiceByRank diceByRank = new DiceByRank();
+            diceByRank.SetField("rank", rank);
+            diceByRank.SetField("diceNumber", dice);
+            return diceByRank;
+        }
+
     }
 }
