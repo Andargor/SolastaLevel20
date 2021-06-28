2eb0b2af270e538b3c61792e5f00d8902d4da886
Paulo Monteiro <pauloesquilo@gmail.com>
2021-06-27 22:22:09 -0700

    researching UI changes

--- SolastaLevel20/Patches/CharacterInformationPanelPatcher.cs
+++ SolastaLevel20/Patches/CharacterInformationPanelPatcher.cs
@@ -1,10 +1,25 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SolastaLevel20.Patches
 {
     class CharacterInformationPanelPatcher
     {
+        [HarmonyPatch(typeof(CharacterInformationPanel), "EnumerateFeatures")]
+        internal static class CharacterInformationPanel_EnumerateFeatures_Patch
+        {
+            internal static void Prefix(CharacterInformationPanel __instance,
+                                        RectTransform table,
+                                        List<FeatureUnlockByLevel> features,
+                                        int currentLevel,
+                                        string insufficientLevelFormat,
+                                        TooltipDefinitions.AnchorMode tooltipAnchorMode)
+            {
+                var x = 0;
+            }
+        }
+
         [HarmonyPatch(typeof(CharacterInformationPanel), "EnumerateClassBadges")]
         internal static class CharacterInformationPanel_EnumerateClassBadges_Patch
         {
