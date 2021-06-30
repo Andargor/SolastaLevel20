3812de28d93551b1768d6f14161baf2bc50bdf4f
Philip Lee <github@pjlconsultants.co.uk>
2021-06-30 15:30:47 +0100

    Add Paladin improved auras

---
+++ SolastaLevel20/Models/Features/PowerPaladinAuraOfCourage18Builder.cs
@@ -0,0 +1,44 @@
+﻿using SolastaModApi;
+using SolastaModApi.Extensions;
+using static SolastaModApi.DatabaseHelper.FeatureDefinitionPowers;
+
+namespace SolastaLevel20.Models.Features
+{
+    internal sealed class PowerPaladinAuraOfCourage18Builder : BaseDefinitionBuilder<FeatureDefinitionPower>
+    {
+        private static FeatureDefinitionPower _instance;
+
+        const string PowerPaladinAuraOfCourage18Name = "Level20_PowerPaladinAuraOfCourage18";
+        const string PowerPaladinAuraOfCourage18Guid = "d68c46024ea8432b981506a13ae35ecd";
+
+        public PowerPaladinAuraOfCourage18Builder() : base(PowerPaladinAuraOfCourage, PowerPaladinAuraOfCourage18Name, PowerPaladinAuraOfCourage18Guid)
+        {
+            var ed = Definition.EffectDescription;
+
+            // Set to 7 instead of 6 because of range bug.  Change back to 6 if TA fix.
+            ed.SetTargetParameter(7);
+            ed.SetRangeParameter(0);
+            ed.SetRequiresTargetProximity(false);
+
+            Definition.SetOverriddenPower(PowerPaladinAuraOfCourage);
+
+            // TODO: localize
+            Definition.GuiPresentation.Description = "Grant a saving throw bonus to allies withing 30ft.";
+            Definition.GuiPresentation.Title = "Improved Aura of Courage";
+        }
+
+        public static FeatureDefinitionPower Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PowerPaladinAuraOfCourage18Builder().AddToDB();
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+}
---
+++ SolastaLevel20/Models/Features/PowerPaladinAuraOfProtection18Builder.cs
@@ -0,0 +1,43 @@
+﻿using SolastaModApi;
+using SolastaModApi.Extensions;
+using static SolastaModApi.DatabaseHelper.FeatureDefinitionPowers;
+
+namespace SolastaLevel20.Models.Features
+{
+    internal sealed class PowerPaladinAuraOfProtection18Builder : BaseDefinitionBuilder<FeatureDefinitionPower>
+    {
+        private static FeatureDefinitionPower _instance;
+
+        const string PowerPaladinAuraOfProtection18Name = "Level20_PowerPaladinAuraOfProtection18";
+        const string PowerPaladinAuraOfProtection18Guid = "1574c379dfb74cfeb3488209bd3b6d33";
+
+        public PowerPaladinAuraOfProtection18Builder() : base(PowerPaladinAuraOfProtection, PowerPaladinAuraOfProtection18Name, PowerPaladinAuraOfProtection18Guid)
+        {
+            var ed = Definition.EffectDescription;
+
+            // Set to 7 instead of 6 because of range bug.  Change back to 6 if TA fix.
+            ed.SetTargetParameter(7);
+            ed.SetRangeParameter(0);
+            ed.SetRequiresTargetProximity(false);
+
+            Definition.SetOverriddenPower(PowerPaladinAuraOfProtection);
+
+            // TODO: localize
+            Definition.GuiPresentation.Description = "You and your allies within 30ft cannot be frightened.";
+            Definition.GuiPresentation.Title = "Improved Aura of Protection";
+        }
+
+        public static FeatureDefinitionPower Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PowerPaladinAuraOfProtection18Builder().AddToDB();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
